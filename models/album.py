ebc34e52072cdc7943447355e398d197ebfe36aa
Calum <calumcmccall@gmail.com>
2022-07-12 14:39:33 +0100

    classes created

---
+++ models/album.py
@@ -0,0 +1,10 @@
+from operator import ge
+
+
+class Album:
+
+    def __init__(self, title, genre, artist, id = None):
+        self.title = title
+        self.genre = genre
+        self.artist = artist
+        self.id = id
