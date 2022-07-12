2f02a1c65fe52cf9b8e492441ba0730bdd548414
Calum <calumcmccall@gmail.com>
2022-07-12 14:33:05 +0100

    db files created

---
+++ db/music_app.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    artist_name VARCHAR(255)
+);
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(255),
+    artist_id INT NOT NULL REFERENCES artists(id)
+);
